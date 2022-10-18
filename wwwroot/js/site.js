d47a3fa1ce7b4d67443f5a9b0793ca5f5028cd46
DarkApple55 <axelcy@gmail.com>
2022-10-18 13:24:21 -0300

    no-drag(no terminado)

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,10 +1,19 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+﻿//#region 
+// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-// substr(0, 10) - agarra los primeros 10 caracteres (1234-67-89)
+//#endregion
 
-document.getElementById('versus-img').setAttribute('draggable', false);
+//# substr(0, 10) - agarra los primeros 10 caracteres (1234-67-89)
+
+document.getElementById('versus-img')?.setAttribute('draggable', false);
+
+// TODAVÍA NO FUNCA
+// let noDragElements = document.getElementsByClassName('no-drag')
+// noDragElements.forEach(element => {
+//     element?.setAttribute('draggable', false);
+// });
 
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
